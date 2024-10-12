9d485750fb331ae692d01360836c95c517283439
Sainath12204 <119676811+Sainath12204@users.noreply.github.com>
2024-10-12 21:32:12 +0530

    Created Flutter project intergrated with firebase

---
+++ remote_sensing/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:remote_sensing/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:remote_sensing/pages/home_page.dart';
+import 'package:remote_sensing/pages/login_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/home': (context) => HomePage(
+            user: FirebaseAuth.instance.currentUser!), // Example of home route
+      },
+      home: LoginPage(), // Set SignInPage as the default page
+    );
+  }
+}
